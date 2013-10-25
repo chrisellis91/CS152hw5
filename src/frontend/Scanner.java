2d9de536526ccd8ab21021026ed77a6efc35caf1
Christopher Ellis <Chris.ellis91@yahoo.com>
2013-10-25 11:05:35 -0700

    General Updates
    Massive changes to scanner/Token. Getting the token class to recognize
    special symbols. Tried to comment most/everything. Much work to be done
    still.

--- src/frontend/Scanner.java
+++ src/frontend/Scanner.java
@@ -2,64 +2,120 @@ package frontend;
 
 import static frontend.Source.EOF;
 
+
+
 import backend.*;
 import intermediate.*;
 import frontend.*;
 
+public class Scanner {
 
-public class Scanner 
-{
+	private Source source; // source
+	private Token currentToken; // current token
 
-	private Source source;     // source
-    private Token currentToken;  // current token
-	
-	
-    
-    
-    public Scanner(Source bSource)
-    {
-        source = bSource;
-    }
-    
-    
-    
-    public char currentChar() throws Exception
-    {
-       return source.currentChar();
-    }
-    
-    
-    
-	public Token nextToken() throws Exception
-	{
-	    currentToken = extractToken();
-	    return currentToken;
+	/**
+	 * This is the constructor for the scanner
+	 * 
+	 * @param bSource
+	 *            the source to use
+	 */
+	public Scanner(Source bSource) {
+		source = bSource;
 	}
-	
-	
-	
-	
-	 private Token extractToken() throws Exception
-	 {
-		  Token token;
-		  char currentChar = currentChar();
 
-	      // Construct the next token.  The current character determines the
-		  // token type.
-		  if (currentChar == Source.EOF) 
-		  {
-		  // token = new EofToken(source);
-		  }
-		  else 
-		  {
-	      //token = new Token(source);
-		  }
+	/**
+	 * This returns the current character that the scanner is on (through use of
+	 * the source class's method)
+	 * 
+	 * @return the current character the scanner is on
+	 * @throws Exception
+	 */
+	public char currentChar() throws Exception {
+		return source.currentChar();
+	}
 
-		  token=null;
-		  return token;
-	 }
-	
-	
-	
-	
+	/**
+	 * This gets the next token from the parser
+	 * 
+	 * @return the next token in the sequence/file
+	 * @throws Exception
+	 */
+	public Token nextToken() throws Exception {
+		currentToken = extractToken();
+		return currentToken;
+	}
+
+	/**
+	 * This extracts the token from parser
+	 * 
+	 * @return the constructed token
+	 * @throws Exception
+	 */
+	protected Token extractToken() throws Exception {
+		skipWhiteSpace();
+
+		Token token;
+		char currentChar = currentChar();
+
+		// Construct the next token. The current character determines the
+		// token type.
+		if (currentChar == EOF) 
+		{
+			token = new EofToken(source);
+		} 
+		else if (Character.isLetter(currentChar)) 
+		{
+			token = new PascalWordToken(source);
+		} 
+		else if (Character.isDigit(currentChar)) 
+		{
+			token = new PascalNumberToken(source);
+		} 
+		else if (currentChar == '\'') 
+		{
+			token = new PascalStringToken(source);
+		} 
+		else if (PascalTokenType.SPECIAL_SYMBOLS.containsKey(Character.toString(currentChar))) 
+		{
+			token = new PascalSpecialSymbolToken(source);
+		}
+		else 
+		{
+			token = new PascalErrorToken(source, INVALID_CHARACTER, Character.toString(currentChar));
+			nextChar(); // consume character
+		}
+
+		return token;
+	}
+
+	/**
+	 * Skip whitespace characters by consuming them. A comment is whitespace.
+	 * 
+	 * @throws Exception
+	 *             if an error occurred.
+	 */
+	private void skipWhiteSpace() throws Exception {
+		char currentChar = currentChar();
+
+		while (Character.isWhitespace(currentChar) || (currentChar == '{')) {
+
+			// Start of a comment?
+			if (currentChar == '{') {
+				do {
+					currentChar = nextChar(); // consume comment characters
+				} while ((currentChar != '}') && (currentChar != EOF));
+
+				// Found closing '}'?
+				if (currentChar == '}') {
+					currentChar = nextChar(); // consume the '}'
+				}
+			}
+
+			// Not a comment.
+			else {
+				currentChar = nextChar(); // consume whitespace character
+			}
+		}
+	}
+
 }
--- src/frontend/Token.java
+++ src/frontend/Token.java
@@ -1,12 +1,194 @@
 package frontend;
 
+
+import java.util.HashSet;
+
+
 import backend.*;
 import intermediate.*;
 import frontend.*;
 
-
+/**
+ * This is a class that represents the token type, much of this
+ * code was reused from Ronald Mak's 153 class with modifications
+ * by team CCT
+ */
 public class Token
 {
+	
+	
+	
+	
+	/**
+	 * Holds all of the reserved words for scheme, as well
+	 * as some basic getter methods/set methods
+	 */
+	public enum TokenType 
+	{
+		// Special symbols.
+	    PLUS("+"), MINUS("-"), STAR("*"), SLASH("/"), COLON_EQUALS(":="),
+	    DOT("."), COMMA(","), SEMICOLON(";"), COLON(":"), QUOTE("'"),
+	    EQUALS("="), NOT_EQUALS("<>"), LESS_THAN("<"), LESS_EQUALS("<="),
+	    GREATER_EQUALS(">="), GREATER_THAN(">"), LEFT_PAREN("("), RIGHT_PAREN(")"),
+	    LEFT_BRACKET("["), RIGHT_BRACKET("]"), LEFT_BRACE("{"), RIGHT_BRACE("}"),
+	    UP_ARROW("^"), DOT_DOT("..");
+	    
+	    
+	    private static final int FIRST_SYMBOL_INDEX = PLUS.ordinal();  //Start of special symbols
+	    private static final int LAST_SYMBOL_INDEX  = DOT_DOT.ordinal(); //End of special symbols
+	   
+	    private String text;  // token text
+	    public static HashSet<String> SPECIAL_SYMBOLS = new HashSet<String>(); //List containing special symbols
+	    
+	    
+	    /**
+	     * A constructor that makes a new token with it's text as it's symbol
+	     * @param myName the text to be used as a symbol
+	     */
+	    TokenType(String myName)
+	    {
+	        this.text = this.toString().toLowerCase();
+	    }
+	    
+	    /**
+	     * This is a getter method that returns the text of the token/symbol
+	     * @return the text/name/symbol for the token
+	     */
+	    public String GetText()
+	    {
+	        return text;
+	    }
+	    
+	    /**
+	     * This puts all of the special symbols into their list
+	     */
+	    static 
+	    {
+	        TokenType values[] = TokenType.values();
+	        for (int i = FIRST_SYMBOL_INDEX; i <= LAST_SYMBOL_INDEX; ++i) 
+	        {
+	        	SPECIAL_SYMBOLS.add(values[i].GetText().toLowerCase());
+	        }
+	    }
+	        	    
+	}
+	
+	
+	    protected TokenType type;  // language-specific token type
+	    protected String text;     // token text
+	    protected Object value;    // token value
+	    protected Source source;   // source
+	    protected int lineNum;     // line number of the token's source line
+	    protected int position;    // position of the first token character
+
+	    /**
+	     * Constructor.
+	     * @param source the source from where to fetch the token's characters.
+	     * @throws Exception if an error occurred.
+	     */
+	    public Token(Source source)
+	        throws Exception
+	    {
+	        this.source = source;
+	        this.lineNum = source.getLineNum();
+	        this.position = source.getPosition();
+
+	        extract();
+	    }
+
+	    /**
+	     * Getter
+	     * @return the token type
+	     */
+	    public TokenType getType()
+	    {
+	        return type;
+	    }
+
+	    /**
+	     * Getter.
+	     * @return the token text.
+	     */
+	    public String getText()
+	    {
+	        return text;
+	    }
+
+	    /**
+	     * Getter.
+	     * @return the token value.
+	     */
+	    public Object getValue()
+	    {
+	        return value;
+	    }
+
+	    /**
+	     * Getter.
+	     * @return the source line number.
+	     */
+	    public int getLineNumber()
+	    {
+	        return lineNum;
+	    }
+
+	    /**
+	     * Getter.
+	     * @return the position.
+	     */
+	    public int getPosition()
+	    {
+	        return position;
+	    }
+
+	    /**
+	     * Default method to extract only one-character tokens from the source.
+	     * Subclasses can override this method to construct language-specific
+	     * tokens.  After extracting the token, the current source line position
+	     * will be one beyond the last token character.
+	     * @throws Exception if an error occurred.
+	     */
+	    protected void extract()
+	        throws Exception
+	    {
+	        text = Character.toString(currentChar());
+	        value = null;
+
+	        nextChar();  // consume current character
+	    }
+
+	    /**
+	     * Call the source's currentChar() method.
+	     * @return the current character from the source.
+	     * @throws Exception if an error occurred.
+	     */
+	    protected char currentChar()
+	        throws Exception
+	    {
+	        return source.currentChar();
+	    }
+
+	    /**
+	     * Call the source's nextChar() method.
+	     * @return the next character from the source after moving forward.
+	     * @throws Exception if an error occurred.
+	     */
+	    protected char nextChar()
+	        throws Exception
+	    {
+	        return source.nextChar();
+	    }
+
+	    /**
+	     * Call the source's peekChar() method.
+	     * @return the next character from the source without moving forward.
+	     * @throws Exception if an error occurred.
+	     */
+	    protected char peekChar()
+	        throws Exception
+	    {
+	        return source.peekChar();
+	    }
 
 	
 
